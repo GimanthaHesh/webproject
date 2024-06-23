b4c7d8972741aa92fc26b4fe7e9c9b064b23bfaa
chamithjeewantha <chamithjeewa123@gmail.com>
2024-06-23 22:57:23 +0530

    add canteen route

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ const { PORT } = require("./utils/config");
 const connectDB = require("./database/config");
 const dotenv = require('dotenv');
 const authroutes = require('./routes/auth.routes')
+const canteenroutes = require('./routes/canteen.routes')
 
 dotenv.config();
 connectDB();
 
 app.use("/auth",authroutes)
+app.use("/canteen",canteenroutes)
 
 app.listen(PORT, () => {
   console.log("API is running on port", PORT);
